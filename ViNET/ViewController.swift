824fc1ecb986401a75fbc1fa9fd78a486e00aa85
xddlzx <dilayece@icloud.com>
2025-05-02 14:42:53 +0300

    Initial Commit

---
+++ ViNET/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViNET
+//
+//  Created by Dilay Ece Maral on 2.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViNETTests/ViNETTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViNETTests.swift
+//  ViNETTests
+//
+//  Created by Dilay Ece Maral on 2.05.2025.
+//
+
+import Testing
+@testable import ViNET
+
+struct ViNETTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
